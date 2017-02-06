d37e67ba218ec1de8bca30121b51de30cba51552
rshreyasi <rshreyasi12@gmail.com>
2017-02-06 23:30:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res) {
-   res.send('Article one requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res) {
