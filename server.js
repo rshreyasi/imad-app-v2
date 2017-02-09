8d5fc66d7b7ff6560e5c8f1bfc32e27456a64a1f
rshreyasi <rshreyasi12@gmail.com>
2017-02-09 20:14:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,10 @@ app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName', function(req, res) {
-    var.articleName=req.params.articleName;
+app.get('/:articleName', function(req, res) {
+// articleName ==  article-one
+//articles[articleName] = {} content object for article one
+ var articleName =  req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 
