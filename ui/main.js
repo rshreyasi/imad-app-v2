4035a6f0e311423bb2b6359815bdb1267db09f1c
rshreyasi <rshreyasi12@gmail.com>
2017-02-10 00:05:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New shreyasi';
+//move the image when clicked
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
